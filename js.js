faa85b54f99e6d00c37317d1d95d77dca7eb3563
Anderson Felipe <136264571+andy7070@users.noreply.github.com>
2023-08-21 03:41:25 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,57 @@
+function clickMenu(){
+    if(animes.style.display == 'none'){
+      animes.style.display = 'block'
+    } else {
+      animes.style.display = 'none'
+    }
+     animes.style.transition = ' 1s'
+    }
+    
+    function clickMenu2(){
+    if(animes2.style.display == 'none'){
+    animes2.style.display = 'block'}else{
+    animes2.style.display = 'none'}}
+    
+    function clickMenu3(){
+    if(animes3.style.display == 'none'){
+        animes3.style.display = 'block'
+    } else{
+    animes3.style.display = 'none'}}
+    
+    
+    function clickMenu4(){
+    if(animes4.style.display == 'none'){
+    animes4.style.display = 'block'}else{
+     animes4.style.display = ' none'}}
+    
+     function clickMenu5(){
+    if(animes5.style.display == 'none'){
+     animes5.style.display = 'block' }else{
+      animes5.style.display = 'none' }
+    }
+    
+    
+    function clickMenu6(){
+     if(animes6.style.display == 'none'){
+       animes6.style.display= 'block'}else{
+      animes6.style.display = 'none'}}
+    
+
+
+    function setaEp(){
+      if(epep.style.display == 'none'){
+      epep.style.display = 'block'} else{
+      epep.style.display = 'none'}
+
+      epep.style.transition = '0.7s'
+
+      if(epseta.style.transform == 'rotateX(180deg)'){
+      epseta.style.transform = 'rotateX(0deg)'}else{
+      epseta.style.transform = 'rotateX(180deg)'}
+
+      
+    }
+
+
+
+    
